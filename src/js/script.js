cec52f423ebc59208e82701f4f0fc0d20f0c29dc
Gouraud Luna <luna.gouraud@edu.univ-fcomte.fr>
2025-12-31 13:46:53 +0100

    fix header menu btn

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 
-toggle.addEventListener("click", () => {
-    nav.classList.toggle("open");
-});
+if(toggle && nav){
+    toggle.addEventListener("click", () => {
+        nav.classList.toggle("open");
+        toggle.classList.toggle("open");
+    });
+}
